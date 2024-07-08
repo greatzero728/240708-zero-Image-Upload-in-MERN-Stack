35f10f192994106d64536cf110f3412ca0d7784a
gitzero728 <happyzero728@gmail.com>
2024-07-08 21:08:19 +0900

    added client part and erased two unused gitignore of server and client

---
+++ client/UploadForm.jsx
@@ -0,0 +1,70 @@
+// client/src/UploadForm.js
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const UploadForm = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    symbol: '',
+    description: '',
+    image: null,
+    percentOfPresale: '',
+    amountOfBuy: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleFileChange = (e) => {
+    setFormData({ ...formData, image: e.target.files[0] });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = new FormData();
+    for (const key in formData) {
+      data.append(key, formData[key]);
+    }
+
+    axios.post('http://localhost:5000/upload', data)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  return (
+    <form onSubmit={handleSubmit} encType="multipart/form-data">
+      <div>
+        <label>Name:</label>
+        <input type="text" name="name" value={formData.name} onChange={handleChange} required />
+      </div>
+      <div>
+        <label>Symbol:</label>
+        <input type="text" name="symbol" value={formData.symbol} onChange={handleChange} required />
+      </div>
+      <div>
+        <label>Description:</label>
+        <textarea name="description" value={formData.description} onChange={handleChange} required />
+      </div>
+      <div>
+        <label>Image:</label>
+        <input type="file" name="image" onChange={handleFileChange} required />
+      </div>
+      <div>
+        <label>Percent of Presale:</label>
+        <input type="number" name="percentOfPresale" value={formData.percentOfPresale} onChange={handleChange} required />
+      </div>
+      <div>
+        <label>Amount of Buy:</label>
+        <input type="number" name="amountOfBuy" value={formData.amountOfBuy} onChange={handleChange} required />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default UploadForm;
---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+// client/src/App.js
+import React from 'react';
+import UploadForm from './UploadForm';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Upload Image</h1>
+      <UploadForm />
+    </div>
+  );
+};
+
+export default App;
